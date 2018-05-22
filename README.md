b5437bde51ee9c901c6b3387a1b2a0fdb39230c2
wanghaoming <whimmy@126.com>
2018-05-22 15:15:39 +0800

    add favicon

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Module | Article
 --|--
 spring-boot-filters | [How to Define a Spring Boot Filter?](http://www.baeldung.com/spring-boot-add-filter)
 deploy-into-tomcat | [Deploy a Spring Boot WAR into a Tomcat Server](http://www.baeldung.com/spring-boot-war-tomcat-deploy)
+favicon | [Guide to the Favicon in Spring Boot](http://www.baeldung.com/spring-boot-favicon)
---
+++ favicon/src/main/java/com/ymmihw/spring/boot/Application.java
@@ -0,0 +1,32 @@
+package com.ymmihw.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+//  @Controller
+  static class FaviconController {
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {}
+  }
+
+  @RestController
+  static class MyController {
+    @GetMapping("test")
+    @ResponseBody
+    String returnNoFavicon() {
+      return "test";
+    }
+  }
+}
