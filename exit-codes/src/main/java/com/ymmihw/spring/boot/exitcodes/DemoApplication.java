87ca92fd008aec86a94b370146b596192694755e
wanghaoming <whimmy@126.com>
2018-05-23 09:13:16 +0800

    copy from baeldung

--- exit-codes/src/main/java/com/ymmihw/spring/boot/exitcodes/DemoApplication.java
+++ exit-codes/src/main/java/com/ymmihw/spring/boot/exitcodes/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.ymmihw.spring.boot.exitcodes;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeEvent;
+import org.springframework.boot.ExitCodeExceptionMapper;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DemoApplication implements ExitCodeGenerator {
@@ -18,5 +23,32 @@ public class DemoApplication implements ExitCodeGenerator {
   @Override
   public int getExitCode() {
     return 42;
+  }
+
+  @Bean
+  CommandLineRunner createException() {
+    return args -> Integer.parseInt("test");
+  }
+
+  @Bean
+  ExitCodeExceptionMapper exitCodeToexceptionMapper() {
+    return exception -> {
+      if (exception.getCause() instanceof NumberFormatException) {
+        return 80;
+      }
+      return 1;
+    };
+  }
+
+  @Bean
+  DemoListener demoListenerBean() {
+    return new DemoListener();
+  }
+
+  private static class DemoListener {
+    @EventListener
+    public void exitEvent(ExitCodeEvent event) {
+      System.out.println("Exit code: " + event.getExitCode());
+    }
   }
 }
