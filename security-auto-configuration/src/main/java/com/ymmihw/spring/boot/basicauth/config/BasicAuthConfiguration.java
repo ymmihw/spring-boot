07bca6909ea9b9fcfa599c9973600961c554669a
wanghaoming <whimmy@126.com>
2018-08-15 16:08:07 +0800

    use PasswordEncoder

--- security-auto-configuration/src/main/java/com/ymmihw/spring/boot/basicauth/config/BasicAuthConfiguration.java
+++ security-auto-configuration/src/main/java/com/ymmihw/spring/boot/basicauth/config/BasicAuthConfiguration.java
@@ -1,12 +1,11 @@
 package com.ymmihw.spring.boot.basicauth.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -15,13 +14,9 @@ public class BasicAuthConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and()
-        .withUser("admin").password("admin").roles("USER", "ADMIN");
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("password"))
+        .roles("USER").and().withUser("admin").password("admin").roles("USER", "ADMIN");
   }
 
   @Override
