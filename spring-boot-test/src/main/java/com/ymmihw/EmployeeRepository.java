93b6e00a86d262c1da28ba7e36b8e8c6f1fb1969
wanghaoming <wanghaoming@ctfo.com>
2023-01-31 14:38:49 +0800

    add spring boot test

---
+++ spring-boot-test/src/main/java/com/ymmihw/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.ymmihw;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+  Employee findByName(String name);
+
+  List<Employee> findAll();
+}
